35a80c4a892566a3d5ade1d242f033ef456f9b7e
Kate <z.catherine53@gmail.com>
2019-07-22 21:56:11 +0300

    Дбавлен "интеллектуальный" login/logout, реализована валидация фикстуры

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -1,4 +1,2 @@
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first("SampleText")
-    app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,9 @@
+from model.group import Group
 
 
-def test_delete_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_first("SampleName")
-    app.session.logout()
+def test_modify_group_name(app):
+    app.group.modify_first_group(Group(name="New group"))
+
+
+def test_modify_group_header(app):
+    app.group.modify_first_group(Group(header="New header"))
