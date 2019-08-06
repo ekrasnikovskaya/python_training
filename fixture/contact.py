40f84d5032550218ffaf583fea582775d4c19870
Kate <z.catherine53@gmail.com>
2019-08-06 18:01:18 +0300

    Создан тест соответствия инфромации на главной странице и странице редактирования

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -15,9 +16,13 @@ class ContactHelper:
         self.app.open_home_page()
         self.contact_cache = None
 
+    def select_by_index_to_edit(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_contact_by_index(self, index):
         wd = self.app.wd
-        self.app.open_home_page()
+        # self.app.open_home_page()
         wd.find_elements_by_name("selected[]")[index].click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
@@ -32,7 +37,7 @@ class ContactHelper:
 
     def modify_contact_by_index(self, index, new_data):
         wd = self.app.wd
-        self.app.open_home_page()
+        #self.app.open_home_page()
         wd.find_elements_by_xpath("(.//img[@alt='Edit'])")[index].click()
         self.fill_contact_form(new_data)
         wd.find_element_by_name("update").click()
@@ -49,7 +54,10 @@ class ContactHelper:
         self.change_field_value("lastname", contact.lastname)
         self.change_field_value("nickname", contact.nickname)
         self.change_field_value("company", contact.company)
-        self.change_field_value("mobile", contact.mobile)
+        self.change_field_value("home", contact.homephone)
+        self.change_field_value("work", contact.workphone)
+        self.change_field_value("mobile", contact.mobilephone)
+        self.change_field_value("phone2", contact.secondaryphone)
         self.change_field_value("email", contact.email)
 
     def change_field_value(self, field_name, text):
@@ -66,15 +74,68 @@ class ContactHelper:
 
     contact_cache = None
 
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
             self.contact_cache = []
-            for element in wd.find_elements_by_name("entry"):
-                fields = element.find_elements_by_tag_name("td")
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(firstname=fields[2].text, lastname=fields[1].text, id=id))
+            for row in wd.find_elements_by_name("entry"):
+                cells = row.find_elements_by_tag_name("td")
+                firstname = cells[2].text
+                lastname = cells[1].text
+                address = cells[3].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                all_emails = cells[4].text
+                all_phones = cells[5].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, address=address, id=id,
+                                                  all_emails_from_homepage=all_emails,
+                                                  all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, address=address, id=id, email=email, email2=email2,
+                       email3=email3, homephone=homephone, workphone=workphone,
+                       mobilephone=mobilephone, secondaryphone=secondaryphone)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(homephone=homephone, workphone=workphone,
+                       mobilephone=mobilephone, secondaryphone=secondaryphone)
+
+
+
 
 
 
--- model/contact.py
+++ model/contact.py
@@ -3,13 +3,21 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, nickname=None, company=None, mobile=None, email=None, id=None):
+    def __init__(self, firstname=None,lastname=None, address=None, homephone=None, workphone=None,
+                 mobilephone=None, secondaryphone=None, all_emails_from_homepage=None, all_phones_from_home_page=None,
+                 email=None, email2=None, email3=None, id=None):
         self.firstname = firstname
         self.lastname = lastname
-        self.nickname = nickname
-        self.company = company
-        self.mobile = mobile
+        self.address = address
+        self.homephone = homephone
+        self.workphone = workphone
+        self.mobilephone = mobilephone
+        self.secondaryphone = secondaryphone
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_homepage = all_emails_from_homepage
         self.email = email
+        self.email2 = email2
+        self.email3 = email3
         self.id = id
 
     def __eq__(self, other):
