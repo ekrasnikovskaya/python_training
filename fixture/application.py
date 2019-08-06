d766f886cb8419db07cb44773bbdd7828fdc556f
Kate <z.catherine53@gmail.com>
2019-08-06 23:32:03 +0300

    Реализованы опции командной строки для браузеров и BaseURL

--- fixture/application.py
+++ fixture/application.py
@@ -6,11 +6,19 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,8 +30,8 @@ class Application:
     def open_home_page(self):
         # open home page
         wd = self.wd
-        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("add")) > 0):
-            wd.get("http://localhost/addressbook/")
+        #if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("add")) > 0):
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
