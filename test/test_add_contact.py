b384807ae80cde716881d65462dc607da8118457
Kate <z.catherine53@gmail.com>
2019-08-14 20:17:13 +0300

    Реализована загрузка тестовых данных из файлов .json

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,36 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import string
-import random
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_email():
-    good_symbols = string.ascii_letters + string.digits + "-"*10 + "."*5
-    name = "".join([random.choice(good_symbols) for k in range(random.randrange(10))])
-    mailserver = "".join([random.choice(good_symbols) for k in range(random.randrange(7))])
-    domain = "".join([random.choice(string.ascii_letters) for k in range(random.randrange(5))])
-    return name+"@"+mailserver+"."+domain
-
-
-testdata = [Contact(firstname="", lastname="", address="", email="", homephone="")] + [
-    Contact(firstname=random_string("name", 10), lastname=random_string("lname", 10), address=random_string("ad", 10),
-            email=random_email(), homephone=random_string("phone", 8))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add_new(contact)
     assert (len(old_contacts) + 1) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert (sorted(old_contacts, key=Contact.id_or_maxsize) == sorted(new_contacts, key=Contact.id_or_maxsize))
-
